5d79c2977edefc3ea7a76e40e66e468322c27c79
cemdenizdemir <cemdenizdemir@gmail.com>
2025-06-20 20:01:41 +0300

    fix(navbar): make link work when you clickec box not text

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -15,23 +15,27 @@ export default async function Navbar() {
     <div className="bg-[var(--pri-200)] border-b border-[var(--pri-400)]">
       <ul className="container flex justify-between [&>*>*]:hover:bg-[var(--pri-300)] [&>*>*]:py-2 [&>*>*]:px-4 [&>*]:flex [&>*>*]:flex [&>*>*]:items-center [&>*>*]:h-full   [&>*>*]:cursor-pointer ">
         <div className="flex">
-          <li>
-            <Link href={`/${locale}/`}>{t("homePage")}</Link>
-          </li>
-          <li>
-            <Link href={`/${locale}/categories`}>{t("categories")}</Link>
-          </li>
-          <li>
-            <Link href={`/${locale}/create-blog`}>{t("createBlog")}</Link>
-          </li>
+          <Link href={`/${locale}/`}>
+            <li>{t("homePage")}</li>
+          </Link>
+          <Link href={`/${locale}/categories`}>
+            {" "}
+            <li>{t("categories")}</li>
+          </Link>
+          <Link href={`/${locale}/create-blog`}>
+            {" "}
+            <li>{t("createBlog")}</li>
+          </Link>
         </div>
         <div>
-          <li>
-            <Link href={`/${locale}/login`}>{t("login")}</Link>
-          </li>
-          <li>
-            <Link href={`/${locale}/signup`}>{t("signup")}</Link>
-          </li>
+          <Link href={`/${locale}/login`}>
+            {" "}
+            <li>{t("login")}</li>
+          </Link>
+          <Link href={`/${locale}/signup`}>
+            {" "}
+            <li>{t("signup")}</li>
+          </Link>
           <li>
             <div>
               <LocaleSwitcher />
