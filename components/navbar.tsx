7514742103623dbb30b1afebdfef7d5d5cf7818c
cemdenizdemir <cemdenizdemir@gmail.com>
2025-06-20 10:41:19 +0300

    fix(navbar): fix locale-aware routing for navbar links

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,33 +1,36 @@
 import Link from "next/link";
-// import { useTranslations } from "next-intl";
+// import { useTranslations, useLocale } from "next-intl";
 
-import { getTranslations } from "next-intl/server";
+import { getTranslations, getLocale } from "next-intl/server";
 import LocaleSwitcher from "./LocaleSwitcher";
 
 export default async function Navbar() {
   // const t = useTranslations("navbar");
+  // const locale = useLocale();
+
   const t = await getTranslations("navbar");
+  const locale = await getLocale();
 
   return (
     <div className="bg-[var(--pri-200)] border-b border-[var(--pri-400)]">
       <ul className="container flex justify-between [&>*>*]:hover:bg-[var(--pri-300)] [&>*>*]:py-2 [&>*>*]:px-4 [&>*]:flex [&>*>*]:flex [&>*>*]:items-center [&>*>*]:h-full   [&>*>*]:cursor-pointer ">
         <div className="flex">
           <li>
-            <Link href="/">{t("homePage")} </Link>
+            <Link href={`/${locale}/`}>{t("homePage")}</Link>
           </li>
           <li>
-            <Link href="/categories">{t("categories")} </Link>
+            <Link href={`/${locale}/categories`}>{t("categories")}</Link>
           </li>
           <li>
-            <Link href="/create-blog">{t("createBlog")} </Link>
+            <Link href={`/${locale}/create-blog`}>{t("createBlog")}</Link>
           </li>
         </div>
         <div>
           <li>
-            <Link href="/login">{t("login")} </Link>
+            <Link href={`/${locale}/login`}>{t("login")}</Link>
           </li>
           <li>
-            <Link href="/signup">{t("signup")} </Link>
+            <Link href={`/${locale}/signup`}>{t("signup")}</Link>
           </li>
           <li>
             <div>
